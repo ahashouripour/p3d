dedaf0054b29107fc2875ce1d600753139d8b20e
ahashouripour <ah.ashouripour@gmail.com>
2025-11-04 08:22:16 +0800

    Fix ts-essentials Version Conflict in mock-instance.ts

--- packages/@n8n/db/src/utils/test-utils/mock-instance.ts
+++ packages/@n8n/db/src/utils/test-utils/mock-instance.ts
@@ -1,6 +1,19 @@
 import { Container, type Constructable } from '@n8n/di';
 import { mock } from 'jest-mock-extended';
-import type { DeepPartial } from 'ts-essentials';
+
+// Local DeepPartial type to avoid ts-essentials version conflicts
+// This matches the structure expected by jest-mock-extended
+type DeepPartial<T> = T extends object
+	? {
+			[P in keyof T]?: T[P] extends (infer U)[]
+				? DeepPartial<U>[]
+				: T[P] extends ReadonlyArray<infer U>
+					? ReadonlyArray<DeepPartial<U>>
+					: T[P] extends Function
+						? T[P]
+						: DeepPartial<T[P]>;
+		}
+	: T;
 
 export const mockInstance = <T>(
 	serviceClass: Constructable<T>,
